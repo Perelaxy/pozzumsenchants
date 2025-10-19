c84e167ae23c95dc77333dd442b56c46014bbc95
Perelaxy <tlandberg102@gmail.com>
2025-10-19 20:16:53 +0200

    Initial commit

---
+++ src/main/java/net/pozzums/PozzumsEnchantsClient.java
@@ -0,0 +1,10 @@
+package net.pozzums;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PozzumsEnchantsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
